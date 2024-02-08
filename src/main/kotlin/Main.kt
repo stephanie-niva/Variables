98cb24b973376a42db41969bbaa4987b8e992a11
stephanie-niva <stephanienivaa@gmail.com>
2024-02-08 14:54:48 +0300

    adding a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val studentname = "Stephanie Wangui"
+    println(studentname)
+    var age = 24
+    println(age)
+    val phoneNumber = +254794740819
+    println(phoneNumber)
+    var weightKg = 70
+    println(weightKg)
+    var kenyanCitizen = true
+    println(kenyanCitizen)
